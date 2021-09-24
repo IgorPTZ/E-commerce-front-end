b6a8a6391a6757ca4ff581abdfcffd88bf551b12
Igor <igor.ptz@hotmail.com>
2021-09-23 23:38:59 -0300

    First commit

---
+++ src/app/common/product.ts
@@ -0,0 +1,20 @@
+export class Product {
+
+    sku: string | undefined;
+
+    name: string | undefined;
+
+    description: string | undefined;
+
+    unitPrice: number | undefined;
+
+    imageUrl: string | undefined;
+
+    active: boolean | undefined;
+
+    unitsInStock: number | undefined;
+
+    dateCreated: Date | undefined;
+
+    lastUpdated: Date | undefined;
+}
