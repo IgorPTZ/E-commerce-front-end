2f8aa80c8eb0f156888aef367897c07a2f26ef98
Igor <igor.ptz@hotmail.com>
2021-10-14 23:32:52 -0300

    Update

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -10,11 +10,22 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductListComponent implements OnInit {
 
-  currentCategoryId: any;
+  previousCategoryId: any = 1;
+
+  currentCategoryId: any = 1;
 
   products: Product[] = [];
 
   searchMode: boolean = false;
+
+  // New properties for pagination
+
+  thePageNumber: number = 1;
+
+  thePageSize: number = 10;
+
+  theTotalElements: number = 0;
+  
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) {}
@@ -65,12 +76,35 @@ export class ProductListComponent implements OnInit {
        // Not category id available ... default to category id 1
        this.currentCategoryId = 1;
      }
- 
+
+     /*
+      Check if we have a different category than previous
+      
+      Note: Angular will reuse a component if it is currently being viewed
+
+      If we have a different category id than previous then set thePageNumber back to 1
+     */
+    if(this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
      // Now get the products for the given category id
-     this.productService.getProductList(this.currentCategoryId).subscribe(
-       data => {
-         this.products = data;
-       }
-     )
+     this.productService.getProductListPaginate(this.thePageNumber - 1,
+                                                this.thePageSize,
+                                                this.currentCategoryId)
+                                                .subscribe(this.processResult());                                           
+  }
+
+  processResult() {
+    return (data: { _embedded: { products: Product[]; }; page: { number: number; size: number; totalElements: number; }; }) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
   }
 }
